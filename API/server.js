75ab6d03dd2abc60f9fa3f1984acb96c591136de
Palito <35345583+Palito9007@users.noreply.github.com>
2018-04-27 21:46:40 +0100

    :(

--- API/server.js
+++ API/server.js
@@ -6,8 +6,13 @@ const fs = require('fs');
 
 const bodyParser = require('body-parser');
 
-app.get('./', function () {
+app.get('./', function (req, res) {
 
-    
-    
+    bodyParser =  JSON.parse('./videos.json');
+    return res.send(bodyParser)
 })
+
+
+app.listen(3000, function() {
+    console.log(' O servidor está online. ');
+});
